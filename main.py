0690cfaef92b72a8f2082d4029cb3c608b1c31af
mmamei <marco.mamei@unimore.it>
2024-05-17 13:53:35 +0200

    initial committ

--- main.py
+++ main.py
@@ -141,6 +141,7 @@ def get_data(s):
         blob = bucket.blob('model.joblib')
         blob.download_to_filename('/tmp/model.joblib')
         model = load('/tmp/model.joblib')
+
         for i in range(10):
             yp = model.predict([[r[-1][1],r[-2][1],r[-3][1],0]])
             r.append([len(r),yp[0]])
